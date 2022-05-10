92225e8a5223cdb6e9e4d1fd4f638d74ab8c992e
mciap <mciapski@.gmail.com>
2022-05-10 22:17:54 +0200

    Spock framework - begining

---
+++ src/test/java/CoordinatesAndDIrectionTestSpock.groovy
@@ -0,0 +1,31 @@
+import pl.pak.rover.Coordinates
+import pl.pak.rover.Rover
+import pl.pak.rover.UnknownCommandException
+import spock.lang.Specification
+
+class CoordinatesAndDIrectionTestSpock extends Specification{
+    def "receive starting point" (){
+        when:
+
+        def results = new Rover(inputCoordinates).getCoordinates()
+        then:
+        def expected = receivedCoordinates
+
+        results==expected
+
+        where:
+        inputCoordinates                             || receivedCoordinates
+        new Coordinates(1.0, 1.0, 'N' as char)       || new Coordinates(1.0, 1.0, 'N' as char)
+        new Coordinates(2.0, 1.0, 'N' as char)       || new Coordinates(2.0, 1.0, 'N' as char)
+    }
+
+    def "throws an exception when receives unknown command"(){
+        when:
+        def results = new Rover().receiveCommands('u' as char)
+
+        then:
+
+        thrown UnknownCommandException
+
+    }
+}
