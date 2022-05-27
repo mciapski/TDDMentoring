fdc49286805842b5acb0ce705705b15684b10e6e
mciap <mciapski@.gmail.com>
2022-05-27 12:33:44 +0200

    Refactor

--- src/main/java/pl/pak/rover/Rover.java
+++ src/main/java/pl/pak/rover/Rover.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import pl.pak.rover.commands.Command;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Data
@@ -13,9 +14,10 @@ public class Rover {
   private Coordinates coordinates;
 
 
-
   public void receiveCommands(Character... commands) {
-    Stream.of(commands).forEach(x->coordinates=Command.of(x).execute(coordinates));
+
+    var commandsList = Stream.of(commands).map(Command::of).collect(Collectors.toList());
+    commandsList.forEach(command -> coordinates=command.execute(coordinates));
 //    for (char commandChar: commands) {
 //      coordinates=Command.of(commandChar).execute(coordinates);
 //    }
--- src/test/java/CoordinatesAndDirectionTest.java
+++ src/test/java/CoordinatesAndDirectionTest.java
@@ -1,9 +1,8 @@
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
-import pl.pak.rover.Coordinates;
-import pl.pak.rover.Rover;
-import pl.pak.rover.UnknownCommandException;
+import pl.pak.rover.*;
 import pl.pak.rover.commands.Command;
 
 import java.io.InvalidObjectException;
@@ -11,6 +10,7 @@ import java.io.InvalidObjectException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static pl.pak.rover.Direction.North;
 import static pl.pak.rover.Direction.West;
@@ -29,6 +29,7 @@ import static pl.pak.rover.Direction.West;
 //9. Przenieść testy ruchów podstawowych do klas testujących DONE
 //10. Obsłużyć obroty wg.TDD DONE
 //11. Refaktor receiveCommands w Rover do postaci strumienia(for) DONE
+//12. Napisać klasy testujące dla obrotów
 
 
 public class CoordinatesAndDirectionTest {
@@ -63,6 +64,18 @@ public class CoordinatesAndDirectionTest {
     rover.receiveCommands('B', 'F', 'F','L');
     //then
     assertThat(rover.getCoordinates()).isEqualTo(new Coordinates(0.0, 1.0, West));
+  }
+  @Test
+  public void doesntMoveWhenAnyOfCommandIsIncorrect() {
+    //given
+    var rover = new Rover(new Coordinates(0.0, 0.0, North));
+    //when
+    var throwable = catchThrowable(()->rover.receiveCommands('B', 'F', 'F','U','L'));
+    //then
+    assertThat(throwable)
+        .isInstanceOf(UnknownCommandException.class)
+        .hasMessage("Unknown command: U");
+    assertThat(rover.getCoordinates()).isEqualTo(new Coordinates(0.0, 0.0, North));
   }
 
 
